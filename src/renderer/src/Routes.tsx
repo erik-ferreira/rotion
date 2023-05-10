70bff74956bdaf4a9a63f1e585240cb6b1820531
erik-ferreira <erikdesouzagf@gmail.com>
2023-05-09 23:44:12 -0300

    config layout

--- src/renderer/src/Routes.tsx
+++ src/renderer/src/Routes.tsx
@@ -3,14 +3,16 @@ import { Router, Route } from "electron-router-dom";
 import { Blank } from "./pages/blank";
 import { Document } from "./pages/document";
 
+import { DefaultLayout } from "./layout/default";
+
 export function Routes() {
   return (
     <Router
       main={
-        <>
+        <Route path="/" element={<DefaultLayout />}>
           <Route path="/" element={<Blank />} />
           <Route path="/document" element={<Document />} />
-        </>
+        </Route>
       }
     />
   );
